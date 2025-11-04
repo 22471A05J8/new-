42f834782be38d4d1b080bd6ca89a32985f9af0b
harini <harinisunkara2005@gmail.com>
2025-11-04 21:23:18 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace "your-repo-name" with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '/new/', 
 })
